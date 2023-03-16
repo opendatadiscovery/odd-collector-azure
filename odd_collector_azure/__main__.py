4cfd495bf5cf208f8075b927575fbf1c30284ef8
Pavel Mackarichev <vixtir90@gmail.com>
2023-03-16 13:54:27 +0300

    chore: update __main__ file

--- odd_collector_azure/__main__.py
+++ odd_collector_azure/__main__.py
@@ -1,38 +1,16 @@
-import asyncio
-import logging
-import os
 from pathlib import Path
 
 from odd_collector_sdk.collector import Collector
 
 from odd_collector_azure.domain.plugin import PLUGIN_FACTORY
 
-logging.basicConfig(
-    level=os.getenv("LOGLEVEL", "INFO"),
-    format="[%(asctime)s] %(levelname)s in %(name)s: %(message)s",
-)
-logger = logging.getLogger("odd-collector-azure")
+COLLECTOR_PACKAGE = __package__
+CONFIG_PATH = Path().cwd() / "collector_config.yaml"
 
-
-try:
-
-    loop = asyncio.get_event_loop()
-
-    config_path = Path().cwd() / os.getenv("CONFIG_PATH", "collector_config.yaml")
-
-    root_package = "odd_collector_azure.adapters"
-
+if __name__ == "__main__":
     collector = Collector(
-        config_path=str(config_path),
-        root_package=root_package,
+        config_path=CONFIG_PATH,
+        root_package=COLLECTOR_PACKAGE,
         plugin_factory=PLUGIN_FACTORY,
     )
-
-    loop.run_until_complete(collector.register_data_sources())
-
-    collector.start_polling()
-
-    asyncio.get_event_loop().run_forever()
-except Exception as e:
-    logger.error(e, exc_info=True)
-    asyncio.get_event_loop().stop()
+    collector.run()
